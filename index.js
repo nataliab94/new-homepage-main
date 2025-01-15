54103c450812b51b7e4d941c0b8e8a1eac64d9ed
nataliab94 <164356684+nataliab94@users.noreply.github.com>
2025-01-15 12:27:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
 $(document).ready(function () {
-  // Obsługa hover dla różnych elementów
+
   $('.read_button').hover(
     function () {
-      $(this).addClass('read_button_cursor'); // Dodaj klasę, gdy kursor jest na elemencie
+      $(this).addClass('read_button_cursor');
     },
     function () {
-      $(this).removeClass('read_button_cursor'); // Usuń klasę, gdy kursor zjedzie
+      $(this).removeClass('read_button_cursor'); 
     }
   );
 
   $('.header_block').hover(
     function () {
-      $(this).addClass('header_block_cursor'); // Dodaj klasę, gdy kursor jest na elemencie
+      $(this).addClass('header_block_cursor'); 
     },
     function () {
-      $(this).removeClass('header_block_cursor'); // Usuń klasę, gdy kursor zjedzie
+      $(this).removeClass('header_block_cursor');
     }
   );
 
   $('.title').hover(
     function () {
-      $(this).addClass('title_cursor'); // Dodaj klasę, gdy kursor jest na elemencie
+      $(this).addClass('title_cursor'); 
     },
     function () {
-      $(this).removeClass('title_cursor'); // Usuń klasę, gdy kursor zjedzie
+      $(this).removeClass('title_cursor'); 
     }
   );
 
   $('.button').hover(
     function () {
-      $(this).addClass('button_cursor'); // Dodaj klasę, gdy kursor jest na elemencie
+      $(this).addClass('button_cursor'); 
     },
     function () {
-      $(this).removeClass('button_cursor'); // Usuń klasę, gdy kursor zjedzie
+      $(this).removeClass('button_cursor');
     }
   );
 
   // Otwieranie menu po kliknięciu ikony menu
   $('.icon_menu').on('click', function () {
-    $('.buttons_menu').addClass('active'); // Dodaj klasę `active` do menu
+    $('.buttons_menu').addClass('active');
   });
 
   // Zamykanie menu po kliknięciu ikony zamykania
   $('.icon_close').on('click', function () {
-    $('.buttons_menu').removeClass('active'); // Usuń klasę `active` z menu
+    $('.buttons_menu').removeClass('active'); 
   });
 });
